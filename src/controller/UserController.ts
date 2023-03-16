66d349a453aca71df77c6c5bf6d3ef809de46c85
rodrigo322 <rodrigollrdglucas@gmail.com>
2023-03-16 11:26:59 -0300

    correção

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -8,44 +8,86 @@ export const createUser = async (req: Request, res: Response) => {
 
   const isUserUniqueEmail = await prisma.user.findUnique({
     where: {
-      email
-    }
+      email,
+    },
   });
 
   const isAccessName = await prisma.access.findUnique({
     where: {
-      name: accessName
-    }
-  })
+      name: accessName,
+    },
+  });
 
-  if(!isAccessName) {
-    return res.status(400).json({message: "Esté nivel de acesso não existe"})
+  if (!isAccessName) {
+    return res.status(400).json({ message: "Esté nivel de acesso não existe" });
   }
 
-  if(isUserUniqueEmail) {
-    return res.status(400).json({message: "Já existe um usuário com este email"})
+  if (isUserUniqueEmail) {
+    return res
+      .status(400)
+      .json({ message: "Já existe um usuário com este email" });
   }
-  
-  const hashPassword = await hash(password, 8)
+
+  const hashPassword = await hash(password, 8);
 
   const user = await prisma.user.create({
-    data: { name, email, password: hashPassword, userAccess: {
-      
-    } },
+    data: {
+      name,
+      email,
+      password: hashPassword,
+      userAccess: {
+        create: {
+          Access: {
+            connect: {
+              name: accessName,
+            }
+          }
+        }
+      }
+    },
     select: {
       id: true,
       name: true,
       email: true,
+      userAccess: {
+        select: {
+          Access: {
+            select: {
+              name: true
+            }
+          }
+        }
+      }
     }
-  });
+  })
 
   return res.json(user);
 };
 
-
 export const deleteManyUser = async (req: Request, res: Response) => {
+  await prisma.user.deleteMany();
 
-  await prisma.user.deleteMany()
+  return res.json({ message: "Usuário deletados" });
+};
 
-  return res.json({message: "Usuário deletados"});
+
+export const getAllUser = async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      userAccess: {
+        select: {
+          Access: {
+            select: {
+              name: true
+            }
+          }
+        }
+      }
+    }
+  });
+
+  return res.json(users);
 };
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,13 @@ import { Router } from "express";
 import { createAccess, getAllAccesses } from "./controller/AccessController";
 import { createProduct } from "./controller/ProductController";
 import { createStore, getAllStore } from "./controller/StoreController";
-import { createUser, deleteManyUser } from "./controller/UserController";
+import { createUser, deleteManyUser, getAllUser } from "./controller/UserController";
 
 export const router = Router();
 
 router.post("/user", createUser);
 router.delete("/delete-users", deleteManyUser);
+router.get("/get-all-users", getAllUser);
 
 router.post("/access", createAccess);
 router.get("/accesses", getAllAccesses);
