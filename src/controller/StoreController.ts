86a86a58c2965defef0aa3abea5dcdc41a18de0d
rodrigo322 <rodrigollrdglucas@gmail.com>
2023-03-21 09:30:13 -0300

    autalizando controller de store

--- src/controller/StoreController.ts
+++ src/controller/StoreController.ts
@@ -29,6 +29,68 @@ export const createStore = async (req: Request, res: Response) => {
   return res.json(store);
 };
 
+export const updateStore = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.user;
+    const { storeId } = req.params;
+
+    const isStore = await prisma.store.findUnique({
+      where: {
+        id: storeId,
+      },
+    });
+
+    if (!isStore) {
+      return res.status(404).json({ message: "Loja não encontrada" });
+    }
+
+    if (id !== isStore.userId) {
+      return res.status(400).json({ message: "Usuário não e dono desta Loja" });
+    }
+
+    const store = await prisma.store.update({
+      where: { id: storeId },
+      data: {
+        name,
+      },
+    });
+
+    return res.status(200).json(store);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+export const deleteStore = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.user;
+    const { storeId } = req.params;
+
+    const isStore = await prisma.store.findUnique({
+      where: {
+        id: storeId,
+      },
+    });
+
+    if (!isStore) {
+      return res.status(404).json({ message: "Loja não encontrada" });
+    }
+
+    if (id !== isStore.userId) {
+      return res.status(400).json({ message: "Usuário não e dono desta Loja" });
+    }
+
+    await prisma.store.delete({
+      where: { id: storeId },
+    });
+
+    return res.status(200).json({ message: "Loja deletada com sucesso." });
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
 export const getAllStore = async (req: Request, res: Response) => {
   const stores = await prisma.store.findMany({
     select: {
@@ -50,4 +112,34 @@ export const getAllStore = async (req: Request, res: Response) => {
     },
   });
   return res.json(stores);
+};
+
+export const getUniqueStore = async (req: Request, res: Response) => {
+  try {
+    const { storeId } = req.params;
+
+    const store = await prisma.store.findUnique({
+      where: {
+        id: storeId,
+      },
+      include: {
+        Product: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            amount: true,
+          },
+        },
+      },
+    });
+
+    if (!store) {
+      return res.status(404).json({ message: "Loja não encontrada" });
+    }
+
+    return res.status(200).json(store);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
 };
--- src/router.ts
+++ src/router.ts
@@ -15,7 +15,11 @@ import {
   getAllSalesBySeller,
 } from "./controller/SaleController";
 import { signIn } from "./controller/SessionController";
-import { createStore, getAllStore } from "./controller/StoreController";
+import {
+  createStore,
+  getAllStore,
+  updateStore,
+} from "./controller/StoreController";
 import {
   createUser,
   deleteManyUser,
@@ -26,22 +30,42 @@ import { authMiddleware } from "./middlewares/AuthMiddleware";
 
 export const router = Router();
 
+/**
+ * Rotas do usuário
+ */
 router.post("/user", createUser);
 router.delete("/delete-users", authMiddleware(["adm"]), deleteManyUser);
 router.get("/get-all-users", authMiddleware(["adm"]), getAllUser);
-
 router.get(
   "/get-unique-user",
   authMiddleware(["adm", "Vendedor", "Comprador"]),
   getUniqueUser
 );
 
+/**
+ * Rotas de acessos
+ */
 router.post("/access", authMiddleware(["adm"]), createAccess);
 router.get("/accesses", authMiddleware(["adm", "Vendedor"]), getAllAccesses);
 
+/**
+ * Rotas da loja
+ */
 router.post("/store", authMiddleware(["adm", "Vendedor"]), createStore);
-router.get("/stores", getAllStore);
+router.get(
+  "/stores",
+  authMiddleware(["adm", "Vendedor", "Comprador"]),
+  getAllStore
+);
+router.put(
+  "/update-store/:storeId",
+  authMiddleware(["adm", "Vendedor"]),
+  updateStore
+);
 
+/**
+ * Rotas do produto
+ */
 router.post(
   "/product/:storeId",
   authMiddleware(["adm", "Vendedor"]),
@@ -68,21 +92,25 @@ router.delete(
   deleteProduct
 );
 
+/**
+ * Rotas de autenticação
+ */
 router.post("/sign-in", signIn);
 
+/**
+ * Rotas da venda
+ */
 router.post(
   "/create-sale",
   authMiddleware(["adm", "Vendedor", "Comprador"]),
   createSale
 );
 router.get("/get-all-sales", authMiddleware(["adm"]), getAllSales);
-
 router.get(
   "/get-all-sales-by-buyer",
   authMiddleware(["adm", "Comprador"]),
   getAllSalesByBuyer
 );
-
 router.get(
   "/get-all-sales-by-seller",
   authMiddleware(["adm", "Vendedor"]),
